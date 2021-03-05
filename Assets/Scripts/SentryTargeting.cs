f0b8bf8de8d32c65f65bdeec0e0ccd49243ea55d
Antonio <wapasmith@gmail.com>
2021-03-04 19:21:50 -0500

    SENTRY AI FIX
    + Sentry now correctly aims and shoots at player
          SHOUTOUT TO NICK AND AARON

--- Assets/Scripts/SentryTargeting.cs
+++ Assets/Scripts/SentryTargeting.cs
@@ -6,20 +6,9 @@ public class SentryTargeting : MonoBehaviour
 {
     public Transform target;
 
-    public bool lockRotationX;
-    public bool lockRotationY;
-    public bool lockRotationZ;
-
-    void Start()
-    {
-
-    }
-
     void Update()
     {
         TurnTowardsPlayer();
-        //print("position: " + target);
-        //print("rotation: " + transform.rotation);
     }
 
     public void TurnTowardsPlayer()
@@ -27,16 +16,6 @@ public class SentryTargeting : MonoBehaviour
         Vector3 disToTarget = target.position - transform.position;
         Quaternion targetRotation = Quaternion.LookRotation(disToTarget, Vector3.up);
 
-        Vector3 euler1 = transform.localEulerAngles;
-        Quaternion prevRot = transform.rotation;
-        transform.rotation = targetRotation;
-        Vector3 euler2 = transform.localEulerAngles;
-
-        if (lockRotationX) euler2.x = euler1.x; // revert x to previous value
-        if (lockRotationY) euler2.y = euler1.y; // revert y to previous value
-        if (lockRotationZ) euler2.z = euler1.z; // revert z to previous value
-
-        transform.rotation = prevRot;
-        transform.localRotation = AnimMath.Slide(transform.localRotation, Quaternion.Euler(euler2), .01f);
+        transform.rotation = AnimMath.Slide(transform.rotation, targetRotation, .01f);
     }
 }
